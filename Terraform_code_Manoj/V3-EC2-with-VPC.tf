5ab8782800abee928971897538cf27551afdc60d
manoj <manoj.mohakud@gmail.com>
2023-09-18 04:43:31 +0530

    Initial commit for V3-EC2-with-VPC.tf

---
+++ Terraform_code_Manoj/V3-EC2-with-VPC.tf
@@ -0,0 +1,111 @@
+# Creating EC2 Instance 
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "demo-server" {
+    ami = "ami-0f5ee92e2d63afc18"
+    instance_type = "t2.micro"
+    key_name =  "dpp"
+    //security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+
+   }
+    
+
+
+# Security group for my EC2 Instance 
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id =  aws_vpc.dpp-vpc.id
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "SSH-Port"
+  }
+}
+
+# creating VPC
+resource "aws_vpc" "dpp-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      Name = "dpp-vpc"
+    }
+  
+}
+# creating subnet 01 that is dpp-public-subnet-01
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1a"
+    tags = {
+      Name = "dpp-public-subnet-01"
+    }
+  
+}
+# creating subnet 02 that is dpp-public-subnet-02
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1b"
+    tags = {
+      Name = "dpp-public-subnet-02"
+    }
+  
+}
+
+# Creating Internet Gateway
+
+resource "aws_internet_gateway" "dpp-igw" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    tags = {
+      Name = "dpp-igw"
+    
+    }
+  
+}
+
+# Creatung Route table
+resource "aws_route_table" "dpp-public-rt" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    route  {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpp-igw.id
+    }
+  
+}
+
+# Creatng Route table association for subnet-01
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+    route_table_id = aws_route_table.dpp-public-rt.id
+  
+}
+
+# Creatng Route table association for subnet-02
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+    subnet_id = aws_subnet.dpp-public-subnet-02.id
+    route_table_id = aws_route_table.dpp-public-rt.id
+  
+}
