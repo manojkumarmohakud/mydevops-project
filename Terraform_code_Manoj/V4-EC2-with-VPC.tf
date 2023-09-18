4d45b569cabf86b3a0fdcec582e8d79f9f1c3e39
manoj <manoj.mohakud@gmail.com>
2023-09-19 02:46:15 +0530

    Addedd Security group 8080 for Jenkins port

--- Terraform_code_Manoj/V4-EC2-with-VPC.tf
+++ Terraform_code_Manoj/V4-EC2-with-VPC.tf
@@ -30,7 +30,13 @@ resource "aws_security_group" "demo-sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
